f8998caffb237b2528706dd9927738a7a6c643ab
Marco Tulio Duenas <marcot.dns@gmail.com>
2024-02-20 18:35:37 -0300

    Refactoring whole website

--- public/assets/js/nav-script.js
+++
@@ -1,6 +0,0 @@
-const toggle_button = document.getElementsByClassName('toggle-button')[0]
-const nav_links = document.getElementsByClassName('nav-links')[0]
-
-toggle_button.addEventListener('click', () => {
-  nav_links.classList.toggle('active')
-})
